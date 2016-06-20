f80c94c77bb1323ef7125097e222a2ce008d4c3e
Andrew Shepherd <ashepherd@journaltech.com>
2016-06-20 10:22:11 -0600

    Initial Commit

---
+++ JustWareApiIntro/JustWareApiIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustWareApiIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
