2dcb2713b117393250804715f51b056d9a387491
Andrew Shepherd <ashepherd@journaltech.com>
2016-06-20 10:36:55 -0600

    Add script for running application
    *GetClient()
    *CleanupEntity
    *OutputNameInformation
    *GetNameWithCollections

--- JustWareApiIntro/JustWareApiIntro/Program.cs
+++ JustWareApiIntro/JustWareApiIntro/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JustWareApiIntro.JustWareApi;
 
 namespace JustWareApiIntro
 {
@@ -10,6 +11,102 @@ namespace JustWareApiIntro
 	{
 		static void Main(string[] args)
 		{
+			JustWareApiClient client = GetClient();
+			//Verify that connection is working
+			int nameId = client.GetCallerNameID();
+			Console.WriteLine("Caller NameID: " + nameId);
+
+			//Name Insert
+			NameInsertUtilities insertUtil = new NameInsertUtilities();
+			Name name = insertUtil.InsertName(client);
+			//Phone phone = insertUtil.AddPhones(client, name);
+			//Address address = insertUtil.AddAddresses(client, name);
+			//Email email = insertUtil.AddEmail(client, name);
+			//NameEvent nameEvent = insertUtil.AddNameEvents(client, name);
+			//NameAttribute nameAttribute = insertUtil.AddNameAttributes(client, name);
+			//NameNote nameNote = insertUtil.AddNameNotes(client, name);
+
+			Console.WriteLine("\nNew Name Information:\n");
+			OutputNameInformation(GetNameWithCollections(client, name));
+
+			//Name Update
+			//NameUpdateUtility updateUtil = new NameUpdateUtility();
+			//Name updatedName = updateUtil.UpdateName(client, name);
+			//Phone updatedPhone = updateUtil.UpdatePhones(client, phone);
+
+			//Console.WriteLine("\nUpdated Name Information:\n");
+			//OutputNameInformation(GetNameWithCollections(client, updatedName));
+
+			//NameDelete
+			CleanupEntity(client, name);
+
+			Console.Write("\nPress Enter to finish...");
+			Console.ReadLine();
+		}
+
+		private static Name GetNameWithCollections(JustWareApiClient client, Name name)
+		{
+			List<string> collections = new List<string>();
+			collections.Add("Phones");
+			collections.Add("Addresses");
+			collections.Add("Emails");
+			collections.Add("Events");
+			collections.Add("Attributes");
+			collections.Add("Notes");
+
+			Name nameWithCollections = client.GetName(name.ID, collections);
+
+			return nameWithCollections;
+		}
+
+		private static void OutputNameInformation(Name name)
+		{
+			Console.WriteLine("Last: {0}", name.Last);
+			Console.WriteLine("First: {0}", name.First);
+			Console.WriteLine("DateOfBirth: {0}", name.DateOfBirth);
+			Console.WriteLine("Weight: {0}", name.Weight);
+			Console.WriteLine("DriversLicenseNumber: {0}", name.DriversLicenseNumber);
+
+			if (name.Phones.Count > 0)
+			{
+				Console.WriteLine("Phones: {0}", name.Phones[0].Number);
+			}
+			if (name.Addresses.Count > 0)
+			{
+				Console.WriteLine("Addresses: {0}", name.Addresses[0].StreetAddress);
+			}
+			if (name.Emails.Count > 0)
+			{
+				Console.WriteLine("Emails: {0}", name.Emails[0].Address);
+			}
+			if (name.Events.Count > 0)
+			{
+				Console.WriteLine("Events: {0} {1}", name.Events[0].Title, name.Events[0].EventDate);
+			}
+			if (name.Attributes.Count > 0)
+			{
+				Console.WriteLine("Name Attributes: {0}", name.Attributes[0].TypeCode);
+			}
+			if (name.Notes.Count > 0)
+			{
+				Console.WriteLine("Notes: {0}", name.Notes[0].Notes);
+			}
+		}
+
+		private static void CleanupEntity(JustWareApiClient client, DataContractBase entity)
+		{
+			Console.WriteLine("\nDeleting name...");
+			entity.Operation = OperationType.Delete;
+			client.Submit(entity);
+		}
+
+		private static JustWareApiClient GetClient()
+		{
+			JustWareApiClient client = new JustWareApiClient();
+			client.ClientCredentials.UserName.UserName = @"tc\User";
+			client.ClientCredentials.UserName.Password = "JustWare5";
+
+			return client;
 		}
 	}
 }
