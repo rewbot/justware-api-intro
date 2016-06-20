4c94d61e315dd33dab5c795260f4481977b82095
Andrew Shepherd <ashepherd@journaltech.com>
2016-06-20 16:23:43 -0600

    Add steps to each insert/update method

--- JustWareApiIntro/JustWareApiIntro/NameInsertUtilities.cs
+++ JustWareApiIntro/JustWareApiIntro/NameInsertUtilities.cs
@@ -33,11 +33,11 @@ namespace JustWareApiIntro
 			name.Weight = 150;
 			name.DriversLicenseNumber = "[yourLastName]-12";
 
-			//Submit
+			//Submit Name object and set keys
 			List<Key> keys = client.Submit(name);
-			int nameId = keys[0].NewID;
 
-			//Get name from database
+			//Use keys to get new Name object from database
+			int nameId = keys[0].NewID;
 			Name dbName = client.GetName(nameId, null);
 
 			return dbName;
@@ -80,10 +80,10 @@ namespace JustWareApiIntro
 			phone.Number = "111-111-1111";
 			phone.NameID = nameId;
 
-			//Submit
+			//Submit Phone object and set keys
 			List<Key> keys = client.Submit(phone);
 
-			//Get object from database
+			//Use keys to get new Phone object from database
 			int phoneId = keys[0].NewID;
 			Phone newPhone = client.GetPhone(phoneId);
 
@@ -111,22 +111,32 @@ namespace JustWareApiIntro
 
 		public Email AddEmail(JustWareApiClient client, int nameId)
 		{
+			//Create Email object
 			Email email = new Email();
+
+			//Fill out info - Operation, TypeCode = "BUS", Address, NameID
 			email.Operation = OperationType.Insert;
 			email.TypeCode = "BUS";
 			email.Address = "someone@somewhere.com";
 			email.NameID = nameId;
 
+			//Submit Email and set keys
 			List<Key> keys = client.Submit(email);
+
+			//Use keys to get new Email object from database
 			int emailId = keys[0].NewID;
 			Email newEmail = client.GetEmail(emailId);
 
+			//Return new Email
 			return newEmail;
 		}
 
 		public Address AddAddresses(JustWareApiClient client, int nameId)
 		{
+			//Create Address object
 			Address address = new Address();
+
+			//Fill out info - Operation, TypeCode = "HA", StateCode = "UT", City, Zip, StreetAddress, NameID
 			address.Operation = OperationType.Insert;
 			address.TypeCode = "HA";
 			address.StateCode = "UT";
@@ -135,30 +145,44 @@ namespace JustWareApiIntro
 			address.StreetAddress = "843 South 100 West";
 			address.NameID = nameId;
 
+			//Submit Email and set keys
 			List<Key> keys = client.Submit(address);
+
+			//Use keys to get new Address object from database
 			int addressId = keys[0].NewID;
 			Address newAddress = client.GetAddress(addressId);
 
+			//Return new Address
 			return newAddress;
 		}
 
 		public NameAttribute AddNameAttributes(JustWareApiClient client, int nameId)
 		{
+			//Create NameAttribute object
 			NameAttribute attribute = new NameAttribute();
+
+			//Fill out info - Operation, TypeCode = "WARR", NameID
 			attribute.Operation = OperationType.Insert;
 			attribute.TypeCode = "WARR";
 			attribute.NameID = nameId;
 
+			//Submit NameAttribute and set keys
 			List<Key> keys = client.Submit(attribute);
+
+			//Use keys to get new NameAttribute object from database
 			int attributeId = keys[0].NewID;
 			NameAttribute newAttribute = client.GetNameAttribute(attributeId);
 
+			//Return new NameAttribute
 			return newAttribute;
 		}
 
 		public NameEvent AddNameEvents(JustWareApiClient client, int nameId)
 		{
+			//Create NameEvent object
 			NameEvent nameEvent = new NameEvent();
+
+			//Fill out info - Operation, EventDate = DateTime.Now, EventEndDate = DateTime.Now.AddHours(2), TypeCode = "ARR", NameID
 			nameEvent.Operation = OperationType.Insert;
 			nameEvent.EventDate = DateTime.Now;
 			nameEvent.EventEndDate = DateTime.Now.AddHours(2);
@@ -166,32 +190,46 @@ namespace JustWareApiIntro
 			nameEvent.NameID = nameId;
 			nameEvent.Title = "My Name Event";
 
+			//Submit NameEvent and set keys
 			List<Key> keys = client.Submit(nameEvent);
+
+			//Use keys to get new NameEvent object from database
 			int nameEventId = keys[0].NewID;
 			NameEvent newNameEvent = client.GetNameEvent(nameEventId);
 
+			//Return new NameEvent
 			return newNameEvent;
 		}
 
 		public NameNote AddNameNotes(JustWareApiClient client, int nameId)
 		{
+			//Create NameNote object
 			NameNote note = new NameNote();
+
+			//Fill out info - Operation, DateTaken = DateTime.Now, Notes, TakenBy = client.GetCallerNameID(), NameId
 			note.Operation = OperationType.Insert;
 			note.DateTaken = DateTime.Now;
 			note.Notes = "Some notes";
 			note.TakenBy = nameId;
 			note.NameID = nameId;
 
+			//Submit NameNote and set keys
 			List<Key> keys = client.Submit(note);
+
+			//Use keys to get new NameNote object from database
 			int noteId = keys[0].NewID;
 			NameNote newNote = client.GetNameNote(noteId);
 
+			//Return new NameNote
 			return newNote;
 		}
 
 		public NameTask AddNameTask(JustWareApiClient client, int nameId)
 		{
+			//Create NameTask object
 			NameTask task = new NameTask();
+
+			//Fill out info - Operation, EventDate = DateTime.Now, EventEndDate = DateTime.Now.AddHours(1), TypeCode = "TASK", Title, NameID
 			task.Operation = OperationType.Insert;
 			task.EventDate = DateTime.Now;
 			task.EventEndDate = DateTime.Now.AddHours(1);
@@ -199,10 +237,14 @@ namespace JustWareApiIntro
 			task.Title = "My Name Task";
 			task.NameID = nameId;
 
+			//Submit NameTask and set keys
 			List<Key> keys = client.Submit(task);
+
+			//Use keys to get new NameTask object from database
 			int taskId = keys[0].NewID;
 			NameTask newTask = client.GetNameTask(taskId, null);
 
+			//Return new NameTask
 			return newTask;
 		}
 	}
--- JustWareApiIntro/JustWareApiIntro/NameUpdateUtilities.cs
+++ JustWareApiIntro/JustWareApiIntro/NameUpdateUtilities.cs
@@ -63,76 +63,97 @@ namespace JustWareApiIntro
 			//Submit Address
 			client.Submit(updateAddress);
 
+			//Get and return Address
 			return client.GetAddress(addressId);
 		}
 
 		public Email UpdateEmail(JustWareApiClient client, int emaiId)
 		{
+			//Create Email object
 			Email updateEmail = new Email();
 
+			//Fill out info - Operation, ID, Address, AddressIsChanged
 			updateEmail.Operation = OperationType.Update;
 			updateEmail.ID = emaiId;
 			updateEmail.Address = "myEmail@somewhere.com";
 			updateEmail.AddressIsChanged = true;
 
+			//Submit Email
 			client.Submit(updateEmail);
 
+			//Get and return Email
 			return client.GetEmail(emaiId);
 		}
 
 		public NameEvent UpdateNameEvents(JustWareApiClient client, int nameEventId)
 		{
+			//Create NameEvent
 			NameEvent updateNameEvent = new NameEvent();
 
+			//Fill out info - Operation, ID, Title, TitleIsChanged
 			updateNameEvent.Operation = OperationType.Update;
 			updateNameEvent.ID = nameEventId;
 			updateNameEvent.Title = "My Updated Event";
 			updateNameEvent.TitleIsChanged = true;
 
+			//Submit NameEvent
 			client.Submit(updateNameEvent);
 
+			//Get and return NameEvent
 			return client.GetNameEvent(nameEventId);
 		}
 
 		public NameAttribute UpdateNameAttributes(JustWareApiClient client, int nameAttributeId)
 		{
+			//Create NameAttribute object
 			NameAttribute updateNameAttribute = new NameAttribute();
 
+			//Fill out info - Operation, ID, TypeCode = "INC", TypeCodeIsChanged
 			updateNameAttribute.Operation = OperationType.Update;
 			updateNameAttribute.ID = nameAttributeId;
 			updateNameAttribute.TypeCode = "INC";
 			updateNameAttribute.TypeCodeIsChanged = true;
 
+			//Submit NameAttribute object
 			client.Submit(updateNameAttribute);
 
+			//Get and return NameAttribute
 			return client.GetNameAttribute(nameAttributeId);
 		}
 
 		public NameNote UpdateNameNotes(JustWareApiClient client, int nameNoteId)
 		{
+			//Create NameNote object
 			NameNote updateNameNote = new NameNote();
 
+			//Fill out info - Operation, ID, Notes, NotesIsChanged
 			updateNameNote.Operation = OperationType.Update;
 			updateNameNote.ID = nameNoteId;
 			updateNameNote.Notes = "My updated notes";
 			updateNameNote.NotesIsChanged = true;
 
+			//Submit NameNote
 			client.Submit(updateNameNote);
 
+			//Get and return NameNote
 			return client.GetNameNote(nameNoteId);
 		}
 
 		public NameTask UpdateNameTasks(JustWareApiClient client, int nameTaskId)
 		{
+			//Create NameTask object
 			NameTask updateNameTask = new NameTask();
 
+			//Fill out info - Operation, ID, Title, TitleIsChanged
 			updateNameTask.Operation = OperationType.Update;
 			updateNameTask.ID = nameTaskId;
 			updateNameTask.Title = "My Updated Task";
 			updateNameTask.TitleIsChanged = true;
 
+			//Submit NameTask
 			client.Submit(updateNameTask);
 
+			//Get and return NameTask
 			return client.GetNameTask(nameTaskId, null);
 		}
 	}
